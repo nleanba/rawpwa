ae73e33e4107b5b2993384c2a5b1b029773fed38
Noam Bachmann <noam@helou.ch>
2018-11-24 02:34:26 +0100

    servic-worker-update

--- sw.js
+++ sw.js
@@ -18,10 +18,37 @@ self.addEventListener('install', function (e) {
 });
 
 /* Serve cached content when offline */
+/*
 self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
+            try {
+                return fetch(e.request)
+            }catch (e)  {
+                return response
+            }
         })
+    );
+});*/
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+    let options = {};
+    e.respondWith(fetch(e.request, options)
+        .then((response) => {
+            const resClone = response.clone()
+            return caches.open(cacheName)
+                .then((cache) => {
+                    cache.put(e.request.url, response)
+                    return resClone
+                })
+        })
+        .catch(() => caches.open(cacheName)
+            .then(cache => cache.match(e.request.url)
+                .then((response) => {
+                    return response || fetch(OFFLINE_GIF)
+                })
+            )
+        )
     );
 });
